9716499b2b3e17f00552618b142000663c70d3e2
lucalhost <lucalhost@noreply.codeberg.org>
2026-03-24 12:57:57 +0100

    version and basic main

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
-int main(){
+#include "version.h"
+#include <stdio.h>
+
+int main() {
+    printf("Version: %s (%s)%s, built: %s\n", PROJECT_GIT_DESCRIBE,
+           PROJECT_GIT_COMMIT, PROJECT_GIT_DIRTY ? "-dirty" : "",
+           PROJECT_BUILD_TIMESTAMP);
     return 0;
 }
